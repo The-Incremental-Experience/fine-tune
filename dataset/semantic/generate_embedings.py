d2da5ea9646d9ea1071b3d47672725a23033507f
UpstairsForest <nikita.batalov@uzh.ch>
2023-02-03 21:54:10 +0100

    Working question embeddings and semantic search

--- dataset/semantic/generate_embedings.py
+++ dataset/semantic/generate_embedings.py
@@ -1,7 +1,7 @@
 import json
 import numpy
 from annoy import AnnoyIndex
-from typing import List
+from typing import List, Dict
 
 import cohere
 
@@ -9,47 +9,47 @@ from dataset.tune.data_models import ProcessedDataModel
 
 JSON_IN = "../tune/lala.json"
 INDEX_OUT = "index.ann"
-CUSTOM_MODEL = "medium"
+JSON_OUT = "keke.json"
+MODEL = "large"
 api_key = "wilJVepgbMNVHebtIy8hYVnAQhvoJu5Qkp9UQEW2"
 co = cohere.Client(api_key)
-
 
 with open(JSON_IN, "r") as f:
     data: List[dict] = json.load(f)
 
-ordered_processed_data = [ProcessedDataModel.parse_obj(o) for o in data[:2]]
+ordered_processed_data = [ProcessedDataModel.parse_obj(o) for o in data]
 ordered_processed_data = [o for o in ordered_processed_data if len(o.questions) > 0]
 
-questions = [f"{i+1}. {o.questions[0]}" for i, o in enumerate(ordered_processed_data)]
+# ordered map of questions to sources
+ordered_question_to_source_map: List[Dict[str, str]] = []
+for o in ordered_processed_data:
+    source = f"{o.title}, {o.chapter} by {o.creator}"
+    for q in o.questions:
+        ordered_question_to_source_map.append({"question": q, "source": source})
 
-questions = ["How fast does a cougar run?"]
-print(questions)
-
-# todo: take all, not just first
 ordered_embeddings = numpy.array(
-    co.embed(texts=questions, model=CUSTOM_MODEL, truncate="RIGHT").embeddings)
-#
-# search_index = AnnoyIndex(ordered_embeddings.shape[1], "angular")
-#
-# for i in range(len(ordered_embeddings)):
-#     search_index.add_item(i, ordered_embeddings[i])
-#
-# search_index.build(n_trees=10)
-# search_index.save(INDEX_OUT)
-#
-# ordered_sources = []
-# for o in ordered_processed_data:
-#     kek = f"{o.title}, {o.chapter} by {o.creator}"
-#     ordered_sources.append(kek)
-#
-# # test
-#
-# sample_question = "What are some emotions that narcissists bury?"
-#
-# question_embedding = numpy.array(co.embed(texts=[sample_question], model=CUSTOM_MODEL).embeddings)[0]
-#
-# similar_item_ids = search_index.get_nns_by_vector(question_embedding, 10,
-#                                                   include_distances=True)
-#
-# for (i, distance) in similar_item_ids:
-#     print(ordered_sources[i], distance)
+    co.embed(texts=[o["question"] for o in ordered_question_to_source_map], model=MODEL, truncate="RIGHT").embeddings)
+
+search_index = AnnoyIndex(ordered_embeddings.shape[1], "angular")
+
+for i in range(len(ordered_embeddings)):
+    search_index.add_item(i, ordered_embeddings[i])
+
+search_index.build(n_trees=10)
+search_index.save(INDEX_OUT)
+with open(JSON_OUT, "w") as f:
+    json.dump(ordered_question_to_source_map, f)
+
+# test
+
+sample_question = "What are some emotions that narcissists hide?"
+
+question_embedding = numpy.array(co.embed(texts=[sample_question], model=MODEL).embeddings)[0]
+
+similar_item_ids = search_index.get_nns_by_vector(question_embedding, 10,
+                                                  include_distances=True)
+
+for i in range(len(similar_item_ids[0])):
+    index = similar_item_ids[0][i]
+    distance = similar_item_ids[1][i]
+    print(ordered_question_to_source_map[index]["question"], distance)
